f8a8dac01067213df777f9dc1fcc73013c781eff
Callum Wellwood-Kane <callum@mudbath.com.au>
2023-07-05 17:11:54 +1000

    move query client

--- src/components/ReactQueryClient.tsx
+++ src/components/ReactQueryClient.tsx
@@ -2,8 +2,8 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode } from "react";
 
+const queryClient = new QueryClient();
 export function ReactQueryClient({ children }: { children: ReactNode }) {
-  const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
